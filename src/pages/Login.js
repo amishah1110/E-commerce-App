e069b551fb67d7ac8155b11575d154ec5e5d4ede
Ami Shah <mailami.shah@gmail.com>
2024-12-20 12:05:15 +0530

    UI changes and forgot password and signup options created

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -10,12 +10,12 @@ const Login = () => {
     return (
         <section id='login'>
             <div className="mx-auto container p-4">
-                <div className='p-2 py-5 bg-white w-full max-w-md mx-auto'> {/*This is the login creds section*/}
+                <div className='p-4 py-5 bg-white w-full max-w-sm mx-auto'> {/*This is the login creds section*/}
                     <div className="w-20 h-20 mx-auto">
                         <img src={loginIcon} alt="login-icon"></img>
                     </div>
 
-                    <form>
+                    <form className='pt-6 '>
                         <div className="grid">
                             <label>Email: </label>
                             <div className="bg-slate-200 p-2">
@@ -38,8 +38,9 @@ const Login = () => {
                             <Link to={"/forgot-password"} className='block w-fit ml-auto hover:underline hover:text-red-600'>Forgot Password</Link> 
                 
                         </div>
-                        <button className="bg-green-800 text-white px-6 py-2 rounded-full w-full max-w-[150px] hover:scale-110 transition-all mx-auto block mt-6">Login</button>
+                        <button className="bg-green-800 text-white px-6 py-2 rounded-full w-full max-w-[150px] hover:scale-110 hover:bg-green-900 transition-all mx-auto block mt-6">Login</button>
                     </form>
+                    <p className="my-5">Don't have an account? <Link to={"/sign-up"} className="hover:underline hover:text-red-700">Click here to Sign Up</Link></p>
 
                 </div>
 
